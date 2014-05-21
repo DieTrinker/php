21d687b5aca3893f950cbc6ddbb171fa5910c6a7
kaffeetrinkerMD <h.kluschke@gmx.de>
2014-05-21 15:47:23 +0200

    Stand 15:15 Uhr

--- php/tutorial-03/php/seite2.php
+++ php/tutorial-03/php/seite2.php
@@ -1,11 +1,68 @@
 <!DOCTYPE unspecified PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <body>
-    <h1>Gästebuch</h1>
-    <p>
-        Hier können Sie sich in das <a href="index.php?seite=3">Gästebuch</a> eintragen</p>
+	<h1>Gästebuch</h1>
+	<p>
+		Hier können Sie sich in das <a href="index.php?seite=3">Gästebuch</a>
+		eintragen
+	</p>
 </body>
 
 </html>
 <?php
-/*     Quellcode für das Gästebuch */
+/* Quellcode für das Gästebuch */
+require 'php/mysql.inc.php';
+
+// Abfrage formulieren
+$abfrage = "
+        SELECT
+            *
+        FROM
+        eintraege
+        ORDER BY
+        datum DESC
+        ";
+// Abfrage an den Server senden
+$ergebnis = mysql_query ( $abfrage, $link );
+
+$anzahl = mysql_num_rows ( $ergebnis );
+
+if ($anzahl > 0) {
+	echo "<p>Anzahl der Gästebucheinträge: " . $anzahl . "</p>";
+} else {
+	echo "<p>Es sind leider noch keine Gästebucheinträge vorhanden!</p>";
+}
+
+/**
+ * alle Datensätze ausgeben...
+ * gitub schlupp...
+ */
+
+/*
+ * while ($datensatz = mysql_fetch_array($ergebnis, MYSQL_ASSOC)){ echo "||name: ".$datensatz['name'] ."|| ++ " ."||datum: ".$datensatz['datum'] ."|| ++ " ."||eintrag: ".$datensatz['eintrag'] ."|| ++ " ."||email: ".$datensatz['email'] ."|| ++ " ."||url: ".$datensatz['url'] ."||<br><br>"; }
+ */
+// Ausgabe aller Gästebucheinträge durch das DIV-Layout
+
+?>
+<!-- Umschlag für einen Gästabucheintrag -->
+<?php while ($datensatz = mysql_fetch_array($ergebnis, MYSQL_ASSOC)){ ?>
+<div class="gb-eintrag">
+	<div class="gb-meta">
+		<!-- Name, Datum, Email, URL -->
+	<?php echo $datensatz['name']; ?>
+	schrieb am:
+	<?php  echo date('d.m.Y - H:i', strtotime($datensatz['datum'])).' Uhr'; ?>
+	(Email:
+	<?php  echo $datensatz['email']; ?>
+	,
+	URL:
+	<?php  echo $datensatz['url']; ?>
+	)
+	</div>
+	<div class="gb-content">
+		<!-- Eintrag -->
+	<?php  echo $datensatz['eintrag']; ?>
+	</div>
+</div>
+}
+<?php }?>
--- php/tutorial-03/php/seite3.php
+++ php/tutorial-03/php/seite3.php
@@ -5,97 +5,85 @@
 // (Eintraege-Button gedrückt)?
 if (isset($_POST['eintragen'])) {
     // auswerten und Fehler(?) festellen
-    
+
     // Auswertung: Name
     if (! empty($_POST['benutzer'])) {
         $name = trim($_POST['benutzer']);
     }
-    
+
     if (! (isset($name) && $name != "")) {
         $error['name'] = "Name muss eingegeben werden!";
     }
-    
+
     // Auswertung: Kommentar
     if (! empty($_POST['eintrag'])) {
         $eintrag = trim($_POST['eintrag']);
     }
-    
+
     if (! (isset($name) && $eintrag != "")) {
         $error['eintrag'] = "Kommentar muss eingegeben werden!";
     }
-    
+
     // Auswertung: Email
     // Funktion: http://php.net/manual/de/function.filter-var.php
     if (! empty($_POST['email'])) {
         $email = trim($_POST['email']);
     }
-    
+
     if (! (isset($email) && filter_var($email, FILTER_VALIDATE_EMAIL))) {
         $error['email'] = "Bitte die Email-Adresse prüfen!";
     }
-    
+
     // Auswertun: URL (eigene Homepage)
     $url = trim($_POST['url']);
-    
+
     // Falls die URL nicht leer ist, muss sie in einem gültigen Format vorliegen
     // gültiges Format: <Protokoll>://<Adresse>
     if ($url != "" && ! filter_var($url, FILTER_VALIDATE_URL)) {
         $error['url'] = "Bitte die URL-Adresse prüfen!";
     }
-    
+
     /*
      * Test der Funktion filter_var() var_dump(filter_var('h.kluschke@gmx.de',
-     * FILTER_VALIDATE_EMAIL)); var_dump(filter_var('nonsens',
-     * FILTER_VALIDATE_EMAIL));
-     */
-    
+             * FILTER_VALIDATE_EMAIL)); var_dump(filter_var('nonsens',
+                     * FILTER_VALIDATE_EMAIL));
+    */
+
     // testen der Validierung
     if (isset($error)) {
         echo "<hr>";
         var_dump($error);
         echo "<hr>";
     }
+
+    include 'php/mysql.inc.php';
+
+    // DB-Abfrage formulieren und zum Server senden
+    $abfrage = "
+            INSERT INTO
+            eintraege
+            (
+            name,
+            datum,
+            eintrag,
+            email,
+            url,
+            ip
+            )
+            VALUES
+            (
+            '" . $name . "',
+                    now(),
+                    '".$eintrag."',
+                            '" . $email . "',
+                            '" . $url . "',
+                                    '" . $_SERVER['REMOTE_ADDR'] . "'
+                                            )
+                                            ";
+    echo $abfrage;
+
+    mysql_query($abfrage);
     
-    // Falls bis hier hin alles in Ordnung ist, dann können wir etwas in die DB
-    // eintragen
-    if (! isset($error)) {
-        // Verbindung zum DB-Server herstellen
-        $link = mysql_connect('localhost', 'benutzer', 'benutzer');
-        
-        if (! $link) {
-            // wir lassen das Script sterben
-            die("Keine Verbindung zum DB-Server möglich!");
-        }
-        
-        // DB auswählen
-        $db = mysql_select_db('db_gbook');
-        
-        if (! $link) {
-            // wir lassen das Script sterben
-            die("Kann die Db nicht verwenden!");
-        }
-        
-        // DB-Abfrage formulieren und zum Server senden
-        $abfrage = "
-                INSERT INTO
-                    eintraege
-                (
-                name,
-                datum,
-                eintrag,
-                email,
-                url,
-                ip
-                )
-                VALUES
-                (
-                '" . $name . "',
-                '" . $email . "',
-                '".$url."',
-                '".$_SERVER['REMOTE_ADDR']."'
-                )
-                ";
-    }
 }
 
 // prüfen, ob wir das Formular anzeigen müssen
